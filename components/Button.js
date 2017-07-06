cb76c42b50910390631a3e931bc60a640057214f
Jonathan Delgado Zamorano <hi@jon.soy>
2017-07-05 22:24:29 -0400

    Update Buttons, add state focus, hover, and disabled

--- components/Button.js
+++ components/Button.js
@@ -1,53 +1,58 @@
 import React from 'react'
+import Color from 'color'
 import toLower  from 'lodash/toLower'
 import * as c from '../libs/colors'
 import TitilliumWeb from './FontFamily'
 import styled, {css} from 'styled-components'
 
 const ButtonStyle = css`
-    font-family: ${TitilliumWeb};
-    font-size: 14px;
-    text-transform: uppercase;
+    border-radius: 13px;
     border: solid 1px black;
     box-sizing: content-box;
-    height: 40px;
-    padding-top: 0px;
-    padding-bottom: 0px;
+    font-family: ${TitilliumWeb};
+    font-size: 14px;
+    height: 26px;
     margin: 0px;
-    border-radius: 20px;
-    padding-left: 24px;
-    padding-right: 24px;
+    padding-bottom: 0px;
     padding-left: 18px;
     padding-right: 18px;
-    height: 26px;
-    border-radius: 13px;
+    padding-top: 0px;
+    text-transform: uppercase;
+
+    &:focus,
+    &.focus {
+        outline: -webkit-focus-ring-color auto 5px;
+        outline-color: -webkit-focus-ring-color;
+        outline-style: auto;
+        outline-width: 5px;
+    }
 `
 
 const ButtonSizeStyle = ({size}) => {
     switch (size) {
         case 'lg':
         case 'large': return css`
+            border-radius: 20px;
+            height: 40px;
             padding-left: 26px;
             padding-right: 26px;
-            height: 40px;
-            border-radius: 20px;
         `
         case 'sm':
         case 'small': return css`
+            border-radius: 10px;
+            font-size: 12px;
+            height: 20px;
             padding-left: 14px;
             padding-right: 14px;
-            height: 20px;
-            font-size: 12px;
-            border-radius: 10px;
         `
         case 'xs':
         case 'extra small':
         case 'extra-small': return css`
+            border-radius: 10px;
+            font-size: 10px;
+            height: 16px;
             padding-left: 10px;
             padding-right: 10px;
-            height: 16px;
-            font-size: 10px;
-            border-radius: 10px;
         `
         // 'medium'
         default: return css`
@@ -61,36 +66,83 @@ const ButtonTypeStyle = ({type}) => {
             color: ${c.WHITE};
             border-color: ${c.BLUE};
             background-color: ${c.BLUE};
+
+            &:focus,
+            &.focus,
+            &:hover,
+            &.hover {
+                background-color: ${Color(c.BLUE).lighten(0.5).string()};
+            }
+
+            &[disabled],
+            &.disabled {
+                color: ${c.GREY};
+                background-color: ${Color(c.BLUE).alpha(0.8).string()};
+                border-color: ${Color(c.BLUE).alpha(0.8).string()};
+            }
         `
         case 'success': return css`
             color: ${c.WHITE};
             border-color: ${c.GREEN};
             background-color: ${c.GREEN};
+
+            &[disabled],
+            &.disabled {
+                color: ${c.WHITE};
+                background-color: ${Color(c.GREEN).alpha(0.8).string()};
+                border-color: ${Color(c.GREEN).alpha(0.8).string()};
+            }
         `
         case 'warning': return css`
             color: ${c.WHITE};
             border-color: ${c.YELLOW};
             background-color: ${c.YELLOW};
+
+            &[disabled],
+            &.disabled {
+                color: ${c.WHITE};
+                background-color: ${Color(c.YELLOW).alpha(0.8).string()};
+                border-color: ${Color(c.YELLOW).alpha(0.8).string()};
+            }
         `
         case 'info': return css`
             color: ${c.WHITE};
             border-color: ${c.BLUELITE};
             background-color: ${c.BLUELITE};
+
+            &[disabled],
+            &.disabled {
+                color: ${c.WHITE};
+                background-color: ${Color(c.BLUELITE).alpha(0.8).string()};
+                border-color: ${Color(c.BLUELITE).alpha(0.8).string()};
+            }
         `
         case 'link': return css`
             color: ${c.BLUE};
             text-decoration: underline;
             border-color: transparent;
             background-color: transparent;
+
+            &[disabled],
+            &.disabled {
+                color: ${c.GREY};
+            }
         `
         case 'danger': return css`
             color: ${c.WHITE};
             border-color: ${c.RED};
             background-color: ${c.RED};
+
+            &[disabled],
+            &.disabled {
+                color: ${c.WHITE};
+                background-color: ${Color(c.RED).alpha(0.8).string()};
+                border-color: ${Color(c.RED).alpha(0.8).string()};
+            }
         `
         // 'active'
         default: return css`
-        color: ${c.BLUE};
+            color: ${c.BLUE};
             border-color: ${c.BLUE};
             background-color: ${c.WHITE};
         `
@@ -99,5 +151,5 @@ const ButtonTypeStyle = ({type}) => {
 
 export const Button = styled.button.attrs({
     size: prop => toLower(prop.size) || 'medium',
-    type: prop => toLower(prop.type) || 'active'
+    type: prop => toLower(prop.type) || 'active',
 })`${ButtonStyle}${ButtonSizeStyle}${ButtonTypeStyle}`
--- stories/Button.js
+++ stories/Button.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { Button } from '../components/Button'
+import styled from 'styled-components'
+
+const D = styled.div`
+    ${Button} {
+        margin-left: 10px;
+        margin-bottom: 10px;
+    }
+`
 
 storiesOf('Button', module)
     .add('all', () => (
-        <div>
+        <D>
             <p>
                 <Button size='large' type='default'>A button</Button>
                 <Button size='large' type='primary'>A primary button</Button>
@@ -14,6 +22,37 @@ storiesOf('Button', module)
                 <Button size='large' type='warning'>A warning button</Button>
                 <Button size='large' type='danger'>A danger button</Button>
                 <Button size='large' type='link'>A link button</Button>
+
+                <h1>Hover</h1>
+
+                <Button size='large' type='default' className="hover">A button</Button>
+                <Button size='large' type='primary' className="hover">A primary button</Button>
+                <Button size='large' type='success' className="hover">A success button</Button>
+                <Button size='large' type='info' className="hover">A info button</Button>
+                <Button size='large' type='warning' className="hover">A warning button</Button>
+                <Button size='large' type='danger' className="hover">A danger button</Button>
+                <Button size='large' type='link' className="hover">A link button</Button>
+
+                <h1>Focus</h1>
+
+                <Button size='large' type='default' className="focus">A button</Button>
+                <Button size='large' type='primary' className="focus">A primary button</Button>
+                <Button size='large' type='success' className="focus">A success button</Button>
+                <Button size='large' type='info' className="focus">A info button</Button>
+                <Button size='large' type='warning' className="focus">A warning button</Button>
+                <Button size='large' type='danger' className="focus">A danger button</Button>
+                <Button size='large' type='link' className="focus">A link button</Button>
+
+                <h1>Disabled</h1>
+
+                <Button size='large' type='default' disabled>A button</Button>
+                <Button size='large' type='primary' disabled>A primary button</Button>
+                <Button size='large' type='success' disabled>A success button</Button>
+                <Button size='large' type='info' disabled>A info button</Button>
+                <Button size='large' type='warning' disabled>A warning button</Button>
+                <Button size='large' type='danger' disabled>A danger button</Button>
+                <Button size='large' type='link' disabled>A link button</Button>
+
             </p>
             <p>
                 <Button type='default'>A button</Button>
@@ -23,6 +62,37 @@ storiesOf('Button', module)
                 <Button type='warning'>A warning button</Button>
                 <Button type='danger'>A danger button</Button>
                 <Button type='link'>A link button</Button>
+
+
+                <h1>Hover</h1>
+
+                <Button type='default' className="hover">A button</Button>
+                <Button type='primary' className="hover">A primary button</Button>
+                <Button type='success' className="hover">A success button</Button>
+                <Button type='info' className="hover">A info button</Button>
+                <Button type='warning' className="hover">A warning button</Button>
+                <Button type='danger' className="hover">A danger button</Button>
+                <Button type='link' className="hover">A link button</Button>
+
+                <h1>Focus</h1>
+
+                <Button type='default' className="focus">A button</Button>
+                <Button type='primary' className="focus">A primary button</Button>
+                <Button type='success' className="focus">A success button</Button>
+                <Button type='info' className="focus">A info button</Button>
+                <Button type='warning' className="focus">A warning button</Button>
+                <Button type='danger' className="focus">A danger button</Button>
+                <Button type='link' className="focus">A link button</Button>
+
+                <h1>Disabled</h1>
+
+                <Button type='default' disabled>A button</Button>
+                <Button type='primary' disabled>A primary button</Button>
+                <Button type='success' disabled>A success button</Button>
+                <Button type='info' disabled>A info button</Button>
+                <Button type='warning' disabled>A warning button</Button>
+                <Button type='danger' disabled>A danger button</Button>
+                <Button type='link' disabled>A link button</Button>
             </p>
             <p>
                 <Button size='small' type='default'>A button</Button>
@@ -32,6 +102,36 @@ storiesOf('Button', module)
                 <Button size='small' type='warning'>A warning button</Button>
                 <Button size='small' type='danger'>A danger button</Button>
                 <Button size='small' type='link'>A link button</Button>
+
+                <h1>Hover</h1>
+
+                <Button size='small' type='default' className="hover">A button</Button>
+                <Button size='small' type='primary' className="hover">A primary button</Button>
+                <Button size='small' type='success' className="hover">A success button</Button>
+                <Button size='small' type='info' className="hover">A info button</Button>
+                <Button size='small' type='warning' className="hover">A warning button</Button>
+                <Button size='small' type='danger' className="hover">A danger button</Button>
+                <Button size='small' type='link' className="hover">A link button</Button>
+
+                <h1>Focus</h1>
+
+                <Button size='small' type='default' className="focus">A button</Button>
+                <Button size='small' type='primary' className="focus">A primary button</Button>
+                <Button size='small' type='success' className="focus">A success button</Button>
+                <Button size='small' type='info' className="focus">A info button</Button>
+                <Button size='small' type='warning' className="focus">A warning button</Button>
+                <Button size='small' type='danger' className="focus">A danger button</Button>
+                <Button size='small' type='link' className="focus">A link button</Button>
+
+                <h1>Disabled</h1>
+
+                <Button size='small' type='default' disabled>A button</Button>
+                <Button size='small' type='primary' disabled>A primary button</Button>
+                <Button size='small' type='success' disabled>A success button</Button>
+                <Button size='small' type='info' disabled>A info button</Button>
+                <Button size='small' type='warning' disabled>A warning button</Button>
+                <Button size='small' type='danger' disabled>A danger button</Button>
+                <Button size='small' type='link' disabled>A link button</Button>
             </p>
             <p>
                 <Button size='extra-small' type='default'>A button</Button>
@@ -41,6 +141,36 @@ storiesOf('Button', module)
                 <Button size='extra-small' type='warning'>A warning button</Button>
                 <Button size='extra-small' type='danger'>A danger button</Button>
                 <Button size='extra-small' type='link'>A link button</Button>
+
+                <h1>Hover</h1>
+
+                <Button size='extra-small' type='default' className="hover">A button</Button>
+                <Button size='extra-small' type='primary' className="hover">A primary button</Button>
+                <Button size='extra-small' type='success' className="hover">A success button</Button>
+                <Button size='extra-small' type='info' className="hover">A info button</Button>
+                <Button size='extra-small' type='warning' className="hover">A warning button</Button>
+                <Button size='extra-small' type='danger' className="hover">A danger button</Button>
+                <Button size='extra-small' type='link' className="hover">A link button</Button>
+
+                <h1>Focus</h1>
+
+                <Button size='extra-small' type='default' className="focus">A button</Button>
+                <Button size='extra-small' type='primary' className="focus">A primary button</Button>
+                <Button size='extra-small' type='success' className="focus">A success button</Button>
+                <Button size='extra-small' type='info' className="focus">A info button</Button>
+                <Button size='extra-small' type='warning' className="focus">A warning button</Button>
+                <Button size='extra-small' type='danger' className="focus">A danger button</Button>
+                <Button size='extra-small' type='link' className="focus">A link button</Button>
+
+                <h1>Disabled</h1>
+
+                <Button size='extra-small' type='default' disabled>A button</Button>
+                <Button size='extra-small' type='primary' disabled>A primary button</Button>
+                <Button size='extra-small' type='success' disabled>A success button</Button>
+                <Button size='extra-small' type='info' disabled>A info button</Button>
+                <Button size='extra-small' type='warning' disabled>A warning button</Button>
+                <Button size='extra-small' type='danger' disabled>A danger button</Button>
+                <Button size='extra-small' type='link' disabled>A link button</Button>
             </p>
-        </div>
+        </D>
     ))
